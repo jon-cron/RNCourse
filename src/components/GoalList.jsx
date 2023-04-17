ca195ce35406fe72636fc0da810dfd47965c64a0
jon-cron <jonathanacm95@gmail.com>
2023-04-17 11:45:00 -0600

    rendering list

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -3,10 +3,26 @@ import { Text, View, StyleSheet } from "react-native";
 
 const GoalList = ({ goals }) => {
   return (
-    <View>{goals.length > 0 && goals.forEach((g) => <Text>{g}</Text>)}</View>
+    <View>
+      {goals.length > 0 &&
+        goals.map((g) => (
+          <Text style={styles.goal} key={g.index}>
+            {g}
+          </Text>
+        ))}
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  goal: {
+    backgroundColor: "red",
+    margin: 5,
+    width: 250,
+    textAlign: "center",
+    borderRadius: 10,
+    paddingVertical: 3,
+  },
+});
 
 export default GoalList;
