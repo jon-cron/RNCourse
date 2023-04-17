b3958bfc1563b6eecf99ebb2731446c804578a15
jon-cron <jonathanacm95@gmail.com>
2023-04-17 11:58:20 -0600

    flatlist

--- src/components/GoalList.jsx
+++ src/components/GoalList.jsx
@@ -1,17 +1,38 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 const GoalList = ({ goals }) => {
   return (
-    <View>
-      {goals.length > 0 &&
-        goals.map((g) => (
-          <Text style={styles.goal} key={g.index}>
-            {g}
-          </Text>
-        ))}
-    </View>
+    <FlatList
+      horizontal
+      // showsHorizontalScrollIndicator={false}
+      data={goals}
+      keyExtractor={goals.index}
+      renderItem={({ item }) => {
+        return (
+          <TouchableOpacity style={styles.goal}>
+            <Text>{item}</Text>
+          </TouchableOpacity>
+        );
+      }}
+    />
   );
+  // return (
+  //   <View>
+  //     {goals.length > 0 &&
+  //       goals.map((g) => (
+  //         <Text style={styles.goal} key={g.index}>
+  //           {g}
+  //         </Text>
+  //       ))}
+  //   </View>
+  // );
 };
 
 const styles = StyleSheet.create({
@@ -19,6 +40,7 @@ const styles = StyleSheet.create({
     backgroundColor: "red",
     margin: 5,
     width: 250,
+    height: 50,
     textAlign: "center",
     borderRadius: 10,
     paddingVertical: 3,
